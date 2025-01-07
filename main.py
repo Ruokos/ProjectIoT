e661111a4d39bd7bef6dc4736bba1edd970b8ea6
Tristan Janmaat <85563465+Ruokos@users.noreply.github.com>
2025-01-07 01:13:00 +0100

    fout gefixed, trainen van modelen het terugschalen van de output werkt zoals verwacht

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 from sklearn.preprocessing import MinMaxScaler
-from joblib import dump, load
+from joblib import dump
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout
+from keras.layers import LSTM, Dense
 from keras.callbacks import EarlyStopping
 
-numeric_columns = ['temperature', 'pressure', 'humidity', 'year']
+np.set_printoptions(threshold=75)
+
+numeric_columns = ['temperature', 'pressure', 'humidity', 'year', 'hour_sin', 'hour_cos', 'month_sin', 'month_cos']
 #'day'
 #Waarden die we nodig hebben voor het maken van sequences om het model te trainen
 output_feature = "temperature"
 output_feature_index = numeric_columns.index(output_feature)
 #We willen 24 uur in de toekomst de temperatuur voorspellen
-future_days = 24
+future_hours = 24
 #Hiervoor gebruiken we n_steps aantal uur
 n_steps = 72
 #Om te voorspellen gebruiken we de waarden uit numeric_columns, we moeten de length hebben van de array zodat het model weet hoeveel inputs het kan verwachten
@@ -48,17 +50,16 @@ def create_pandas_frame() -> pd.DataFrame:
             humidity = int(values[17])
 
             data = {
-                "date": date,
+                'temperature': temperature,
+                "pressure": pressure,
+                'humidity': humidity,
                 "year": date.year,
-                #"day": date.day,
                 "hour_sin": np.sin(2 * np.pi * date.hour / 24),
                 "hour_cos": np.cos(2 * np.pi * date.hour / 24),
                 "month_sin": np.sin(2 * np.pi * date.month / 12),
                 "month_cos": np.cos(2 * np.pi * date.month / 12),
-                "pressure": pressure,
-                'humidity': humidity,
-                'temperature': temperature
-
+                "date": date,
+                #"day": date.day,
             }
             extracted_data.append(data)
     
@@ -89,26 +90,39 @@ def prepare_data_for_training(data: pd.DataFrame):
     validation_data = data[training_size:training_size + validation_size]
     testing_data = data[-testing_size:]
 
-    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaler = None
 
-    training_data_normalized = scaler.fit_transform(training_data[numeric_columns])
-    validation_data_normalized = scaler.transform(validation_data[numeric_columns])
-    testing_data_normalized = scaler.transform(testing_data[numeric_columns])
+    training_data_normalized = training_data.copy()
+    validation_data_normalized = validation_data.copy()
+    testing_data_normalized = testing_data.copy()
 
+    for column in numeric_columns:
+        temp_scaler = MinMaxScaler(feature_range=(0, 1))
+
+        training_data_normalized[column] = temp_scaler.fit_transform(training_data[[column]])
+        validation_data_normalized[column] = temp_scaler.transform(validation_data[[column]])
+        testing_data_normalized[column] = temp_scaler.transform(testing_data[[column]])
+
+        #We only want to save the scaler used for temperature
+        if column == output_feature:
+            scaler = temp_scaler
+    
+    print(testing_data_normalized)
     return scaler, training_data_normalized, validation_data_normalized, testing_data_normalized
 
 
 def save_dataframe(data: pd.DataFrame, path: str):
     data.to_csv(path, index=True)
 
-def create_sequences(data_input, n_steps, future_days, out_feature_index):
+def create_sequences(data_input, n_steps, future_hours, out_feature_index):
     """
     Functie die sequences maakt van onze data die een LSTM kan gebruiken
     """
+    data_input = data_input.values
     x, y = [], []
-    for i in range(len(data_input) - n_steps - future_days):
+    for i in range(len(data_input) - n_steps - future_hours):
         end_ix = i + n_steps
-        out_end_ix = end_ix + future_days
+        out_end_ix = end_ix + future_hours
         if out_end_ix > len(data_input):
             break
         seq_x, seq_y = data_input[i:end_ix, :], data_input[out_end_ix - 1, out_feature_index]
@@ -119,20 +133,19 @@ def create_sequences(data_input, n_steps, future_days, out_feature_index):
 
 def test_model(model, x_testing, y_testing, scaler):
     predictions = model.predict(x_testing)
+    #padded_predictions = np.zeros((predictions.shape[0], n_inputs))
+    #padded_predictions[:, output_feature_index] = predictions.flatten()
+    predictions_rescaled = scaler.inverse_transform(predictions.reshape(-1, 1))
 
-    padded_predictions = np.zeros((predictions.shape[0], n_inputs))
-    padded_predictions[:, output_feature_index] = predictions.flatten()
-    print(padded_predictions)
-    predictions_rescaled = scaler.inverse_transform(padded_predictions)[:, output_feature_index]
-    print(predictions_rescaled)
+    #padded_y_testing = np.zeros((y_testing.shape[0], n_inputs))
+    #padded_y_testing[:, output_feature_index] = y_testing.flatten()
+    y_testing_rescaled = scaler.inverse_transform(y_testing.reshape(-1, 1))
 
-    padded_y_testing = np.zeros((y_testing.shape[0], n_inputs))
-    padded_y_testing[:, output_feature_index] = y_testing.flatten()
-    y_testing_rescaled = scaler.inverse_transform(padded_y_testing)[:, output_feature_index]
-
+    print(predictions_rescaled, y_testing_rescaled)
     errors = predictions_rescaled - y_testing_rescaled
     mae = np.mean(np.abs(errors))
-    print(f"Lijst van daadwerkelijke waarde vs voorspelde waarde: {errors}\nMean Absolute Error: {mae}")
+    #print(f"Lijst van daadwerkelijke waarde vs voorspelde waarde: {errors}")
+    print(f"Mean Absolute Error: {mae}")
 
 def main():
     data_path = './data/weather_data_api_daily.csv'
@@ -142,9 +155,11 @@ def main():
 
     scaler, training, validation, testing = prepare_data_for_training(dataframe)
 
-    x_train, y_train = create_sequences(training, n_steps, future_days, output_feature_index)
-    x_validation, y_validation = create_sequences(validation, n_steps, future_days, output_feature_index)
-    x_testing, y_testing = create_sequences(testing, n_steps, future_days, output_feature_index)
+    x_train, y_train = create_sequences(training, n_steps, future_hours, output_feature_index)
+    x_validation, y_validation = create_sequences(validation, n_steps, future_hours, output_feature_index)
+    x_testing, y_testing = create_sequences(testing, n_steps, future_hours, output_feature_index)
+
+    print(y_testing)
 
     #Hier maken we het model dat we gaan trainen
     model = Sequential([
@@ -166,14 +181,16 @@ def main():
     history = model.fit(
         x_train, y_train,
         validation_data=(x_validation, y_validation),
-        epochs=50,
-        batch_size=128,
+        epochs=20,
+        batch_size=256,
         callbacks=[early_stop]
     )
 
+    #Model en gebruikte scaler opslaan nadat we het model getrained hebben
+    #De scaler slaan we op zodat we de predictions van het model ook weer terug kunnen rekenen naar graden celsius
     model.save("./model/weather_model.h5")
     dump(scaler, './model/scaler.pkl')
-
+    
     test_model(model, x_testing, y_testing, scaler)
 
 if __name__ == '__main__': 
