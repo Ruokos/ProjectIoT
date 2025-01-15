80a992e666b64e7714324ce09b0f4604d71eae1a
Tristan Janmaat <85563465+Ruokos@users.noreply.github.com>
2025-01-15 01:11:22 +0100

    kleine aanpassingen

--- predict_weather.py
+++ predict_weather.py
@@ -4,7 +4,7 @@ from joblib import load
 import sqlite3 as sql
 import pandas as pd
 import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 
 numeric_columns = ['temperature', 'pressure', 'humidity', 'year', 'hour_sin', 'hour_cos', 'month_sin', 'month_cos']
 
@@ -24,22 +24,29 @@ ORDER BY id DESC
 LIMIT {LIMIT_ROWS};
 """
 
-#Load queried database rows as dataframe
+#Data uit database laden in dataframe
 df = pd.read_sql_query(query, conn)
-df = df.iloc[::-1]
 
-df['datetime'] = pd.to_datetime(df['datetime'])
+#Data in hetzelfde format zetten als waarmee het model is getrained
+df['date'] = pd.to_datetime(df['date'])
+df['hour_sin'] = np.sin(2 * np.pi * df['date'].hour / 24)
+df['hour_cos'] = np.cos(2 * np.pi * df['date'].dt.hour / 24)
+df['month_sin'] = np.sin(2 * np.pi * df['date'].dt.month / 12)
+df['month_cos'] = np.cos(2 * np.pi * df['date'].dt.month / 12)
 
-df['hour_sin'] = np.sin(2 * np.pi * df['datetime'].hour / 24)
-df['hour_cos'] = np.cos(2 * np.pi * df['datetime'].dt.hour / 24)
-df['month_sin'] = np.sin(2 * np.pi * df['datetime'].dt.month / 12)
-df['month_cos'] = np.cos(2 * np.pi * df['datetime'].dt.month / 12)
+#Als index van ons DataFrame gebruiken we de datum
+df.set_index('date', inplace=True)
+df.sort_values(by='date', inplace=True)
 
+#Scalers die gebruikt waren tijdens trainen toepassen op sensordata
 model_input = df[numeric_columns].copy()
 for column in numeric_columns:
     model_input[column] = scalers[column].transform(df[[column]])
 
+#Model temperatuur laten voorspellen
 prediction = weather_model.predict(model_input.to_numpy())
 prediction_rescale = scalers['temperature'].inverse_transform(prediction.reshape(-1, -1))
 
-print(f"Predicted temperature over 24 hours: {prediction_rescale}")
+latest_datetime = df.index[-1]
+prediction_time = latest_datetime + timedelta(hours=24)
+print(f"Predicted temperature at {prediction_time}: {prediction_rescale[0][0]:.1f} °C")
